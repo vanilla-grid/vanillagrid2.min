0467cd0387eb8328c2ca7078afb19ba067d087cb
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-04 16:52:27 +0900

    Create enum.ts

---
+++ src/types/enum.ts
@@ -0,0 +1,28 @@
+export const enum Align {
+    left = "left",
+    center = "center",
+    right = "right"
+}
+export const enum VerticalAlign {
+    top = "top",
+    center = "center",
+    bottom = "bottom"
+}
+export const enum SelectionPolicy {
+    single = "single",
+    range = "range",
+    none = "none"
+}
+export const enum ColorSet {
+    skyblue = "skyblue",
+    blue = "blue",
+    light_red = "light-red",
+    red = "red",
+    light_green = "light-green",
+    green = "green",
+    orange = "orange",
+    yellow = "yellow",
+    purple = "purple",
+    brown = "brown",
+    black = "black"
+}
