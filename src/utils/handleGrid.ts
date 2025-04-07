b5dc1955f1fe56068f341813a86e6a78570c1a8c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-07 17:02:19 +0900

    Update handleGrid.ts

--- src/utils/handleGrid.ts
+++ src/utils/handleGrid.ts
@@ -131,77 +131,76 @@ export const __setGridColSize = (grid: Grid) => {
     grid.gridFooter!.style.gridTemplateColumns = styleGridTemplateColumns;
 };
 export const _getCellChildNode = (cell: Cell): HTMLElement | null => {
-    const grid = cell._grid;
     if (!cell) return null;
     let childNode: any;
     switch (cell._colInfo.dataType) {
         case 'text':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-text');
+            childNode.classList.add(cell._grid._id + '_data-value-text');
             childNode.innerText = cell._value;
             childNode.nType = 'text';
             break;
         case 'number':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-number');
-            if (cell._value === null || cell._value === undefined || cell._value === grid._gridInfo.nullValue) childNode.innerText = grid._gridInfo.nullValue;
+            childNode.classList.add(cell._grid._id + '_data-value-number');
+            if (cell._value === null || cell._value === undefined || cell._value === cell._grid._gridInfo.nullValue) childNode.innerText = cell._grid._gridInfo.nullValue;
             else childNode.innerText = getFormatNumberFromCell(cell);
             childNode.nType = 'number';
             break;
         case 'mask':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-mask');
+            childNode.classList.add(cell._grid._id + '_data-value-mask');
             childNode.innerText = cell._value;
             childNode.nType = 'mask';
             break;
         case 'date':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-date');
-            if (cell._value === null || cell._value === undefined || cell._value === grid._gridInfo.nullValue) childNode.innerText = grid._gridInfo.nullValue;
+            childNode.classList.add(cell._grid._id + '_data-value-date');
+            if (cell._value === null || cell._value === undefined || cell._value === cell._grid._gridInfo.nullValue) childNode.innerText = cell._grid._gridInfo.nullValue;
             else childNode.innerText = getDateWithGridDateFormat(cell);
             childNode.nType = 'date';
             break;
         case 'month':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-month');
-            if (cell._value === null || cell._value === undefined || cell._value === grid._gridInfo.nullValue) childNode.innerText = grid._gridInfo.nullValue;
+            childNode.classList.add(cell._grid._id + '_data-value-month');
+            if (cell._value === null || cell._value === undefined || cell._value === cell._grid._gridInfo.nullValue) childNode.innerText = cell._grid._gridInfo.nullValue;
             else childNode.innerText = getDateWithGridMonthFormat(cell);
             childNode.nType = 'month';
             break;
         case 'select':
             if (Array.isArray(cell._value) && cell._value.length > 0) {
                 childNode = document.createElement('select');
-                childNode.classList.add(grid._id + '_data-value-select');
-                childNode.addEventListener('change', function (e: any) { selectAndCheckboxOnChange(grid, e.target); });
+                childNode.classList.add(cell._grid._id + '_data-value-select');
+                childNode.addEventListener('change', function (e: any) { selectAndCheckboxOnChange(e.target); });
                 setSelectOptions(childNode, cell._value);
                 childNode.nType = 'select';
                 if (cell._colInfo.selectSize) childNode.style.width = cell._colInfo.selectSize;
             }
             else {
                 childNode = document.createElement('span');
-                childNode.classList.add(grid._id + '_data-value-text');
-                childNode.innerText = grid._gridInfo.nullValue;
+                childNode.classList.add(cell._grid._id + '_data-value-text');
+                childNode.innerText = cell._grid._gridInfo.nullValue;
                 childNode.nType = 'text';
             }
             break;
         case 'checkbox':
             childNode = document.createElement('input');
-            childNode.classList.add(grid._id + '_data-value-checkbox');
-            childNode.addEventListener('change', function (e: any) { selectAndCheckboxOnChange(grid, e.target); });
+            childNode.classList.add(cell._grid._id + '_data-value-checkbox');
+            childNode.addEventListener('change', function (e: any) { selectAndCheckboxOnChange(e.target); });
             childNode.type = 'checkbox';
             childNode.nType = 'checkbox';
             if (getCheckboxCellTrueOrFalse(cell)) childNode.setAttribute('checked','');
             break;
         case 'button':
-            if (cell._value === null || cell._value === undefined || cell._value === grid._gridInfo.nullValue) {
+            if (cell._value === null || cell._value === undefined || cell._value === cell._grid._gridInfo.nullValue) {
                 childNode = document.createElement('span');
-                childNode.classList.add(grid._id + '_data-value-text');
-                childNode.innerText = grid._gridInfo.nullValue;
+                childNode.classList.add(cell._grid._id + '_data-value-text');
+                childNode.innerText = cell._grid._gridInfo.nullValue;
                 childNode.nType = 'text';
             }
             else {
                 childNode = document.createElement('button');
-                childNode.classList.add(grid._id + '_data-value-button');
+                childNode.classList.add(cell._grid._id + '_data-value-button');
                 childNode.innerText = cell._value;
                 childNode.nType = 'button';
                 childNode.addEventListener('touchstart', function() {
@@ -213,15 +212,15 @@ export const _getCellChildNode = (cell: Cell): HTMLElement | null => {
             }
             break;
         case 'link':
-            if (cell._value === null || cell._value === undefined || cell._value === grid._gridInfo.nullValue) {
+            if (cell._value === null || cell._value === undefined || cell._value === cell._grid._gridInfo.nullValue) {
                 childNode = document.createElement('span');
-                childNode.classList.add(grid._id + '_data-value-text');
-                childNode.innerText = grid._gridInfo.nullValue;
+                childNode.classList.add(cell._grid._id + '_data-value-text');
+                childNode.innerText = cell._grid._gridInfo.nullValue;
                 childNode.nType = 'text';
             }
             else {
                 childNode = document.createElement('a');
-                childNode.classList.add(grid._id + '_data-value-link');
+                childNode.classList.add(cell._grid._id + '_data-value-link');
                 childNode.innerText = cell._value.text;
                 childNode.setAttribute('href', cell._value.value);
                 childNode.setAttribute('target', cell._value.target ? cell._value.target : '_blank');
@@ -230,17 +229,17 @@ export const _getCellChildNode = (cell: Cell): HTMLElement | null => {
             break;
         case 'code':
             childNode = document.createElement('span');
-            childNode.classList.add(grid._id + '_data-value-code');
+            childNode.classList.add(cell._grid._id + '_data-value-code');
             childNode.innerText = cell._value;
             childNode.nType = 'code';
             break;
         default:
-            if(grid._vg.dataType) {
-                Object.keys(grid._vg.dataType).forEach((key) => {
+            if(cell._grid._vg.dataType) {
+                Object.keys(cell._grid._vg.dataType).forEach((key) => {
                     if(key === cell._colInfo.dataType) {
-                        if(grid._vg.dataType[key].getChildNode) {
-                            if(typeof grid._vg.dataType[key].getChildNode !== 'function') throw new Error('getChildNode must be a function.');
-                            childNode = grid._vg.dataType[key].getChildNode(__getData(cell));
+                        if(cell._grid._vg.dataType[key].getChildNode) {
+                            if(typeof cell._grid._vg.dataType[key].getChildNode !== 'function') throw new Error('getChildNode must be a function.');
+                            childNode = cell._grid._vg.dataType[key].getChildNode(__getData(cell));
                             if(childNode) {
                                 if(!(childNode instanceof HTMLElement) || childNode.nodeType !== 1)  throw new Error('getChildNode must return an html element.');
                             }
@@ -253,14 +252,14 @@ export const _getCellChildNode = (cell: Cell): HTMLElement | null => {
                             childNode = document.createElement('span');
                             childNode.innerText = cell._value;
                         }
-                        childNode.classList.add(grid._id + '_data-value-' + key);
+                        childNode.classList.add(cell._grid._id + '_data-value-' + key);
                         childNode.nType = key;
                     }
                 });
             }
             break;
     }
-    childNode.classList.add(grid._id + '_data-value');
+    childNode.classList.add(cell._grid._id + '_data-value');
     childNode.gType = 'gbdv';
     return childNode;
 };
@@ -528,6 +527,7 @@ export const __checkColIndex = (grid: Grid, col: number) => {
     col = validatePositiveIntegerAndZero(col);
     if (!col || col < 1 || col > grid.getColCount()) throw new Error('Please insert a col of valid range.');
 };
+//수정필요 cell data 형태
 export const ___getDatasWithoutExceptedProperty = (grid: Grid, exceptedProperty = []) => {
     const datas = [];
     let cols;
@@ -594,19 +594,19 @@ export const _doFilter = (grid: Grid) => {
     grid.load(grid.getDatas());
 };
 export const __gridCellReConnectedWithControlSpan = (cell: Cell) => {
-    const grid = cell._grid;
     reConnectedCallbackElement(cell);
     if(cell._rowSpan) {
         for(let row = cell._row + 1; row < cell._row + cell._rowSpan; row++) {
-            __gridCellReConnectedWithControlSpan(_getCell(grid, row, cell._col)!);
+            __gridCellReConnectedWithControlSpan(_getCell(cell._grid, row, cell._col)!);
         }
     }
     if(cell._colSpan) {
         for(let col = cell._col + 1; col < cell._col + cell._colSpan; col++) {
-            __gridCellReConnectedWithControlSpan(_getCell(grid, cell._row, col)!);
+            __gridCellReConnectedWithControlSpan(_getCell(cell._grid, cell._row, col)!);
         }
     }
 };
+//수정필요 cell data 형태
 export const __getData = (cell: Cell, exceptedProperty: string[] = []): CellData => {
     const data: CellData = {
         _gridId : cell._gridId,
@@ -651,8 +651,10 @@ export const __getData = (cell: Cell, exceptedProperty: string[] = []): CellData
         if (exceptedProperty.indexOf('filter') >= 0) data._colInfo.filter = null;
         if (exceptedProperty.indexOf('value') >= 0) data._value = null;
     }
+    data._text = getCellText(cell);
     return data;
 };
+//수정필요 cell data 형태
 export const __setCellData = (grid: Grid, row: number, colIndexOrColId: number | string, cellData: CellData, isImmutableColCheck = true) => {
     __checkRowIndex(grid, row);
     const colIndex = __getColIndex(grid, colIndexOrColId, true)!;
