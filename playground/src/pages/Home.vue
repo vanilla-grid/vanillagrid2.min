8c042c6bb11422c541bb004b4d2d1c5a25ffe40d
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-09 16:53:23 +0900

    Update Home.vue

--- playground/src/pages/Home.vue
+++ playground/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1>Welcome to Vanillagrid Playground</h1>
-      <p>Click the menu to test the editor.</p>
+      <p>Click the menu to test the grid.</p>
     </div>
 </template>
 
