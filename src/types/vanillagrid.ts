af19dc00b9bbab22f3d353875bdfd1e801238b0d
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-10 16:55:15 +0900

    Update vanillagrid.ts

--- src/types/vanillagrid.ts
+++ src/types/vanillagrid.ts
@@ -1,9 +1,9 @@
-import type { DefaultGridCssInfo, DefaultGridInfo, GridCssInfo, GridInfo } from "./gridInfo";
-import type { ColInfo, DefaultColInfo } from "./colInfo";
+import type { DefaultGridCssInfo, DefaultGridInfo } from "./gridInfo";
+import type { DefaultColInfo } from "./colInfo";
 import type { DataType } from "./dataType";
-import type { GridMethods } from "./gridMethods";
 import type { documentEvent } from "./event";
-import type { Cell, CellRecord } from "./cell";
+import type { Grid } from "./grid";
+import type { GridMethods } from "./gridMethods";
 
 /**
  * The main interface representing the Vanillagrid instance.
@@ -25,7 +25,6 @@ import type { Cell, CellRecord } from "./cell";
  * ```
  */
 export interface Vanillagrid extends VanillagridConfig{
-    grids : Record<string, Grid>;
     /**
      * An array containing IDs of all grids managed by this instance.
      */
@@ -42,7 +41,7 @@ export interface Vanillagrid extends VanillagridConfig{
      * @param gridId - Unique identifier of the grid instance.
      * @returns Methods available for managing the specified grid instance.
      */
-    getGrid(gridId: string): Grid;
+    getGrid(gridId: string): GridMethods | null;
 
     init(): void,
     /**
@@ -155,75 +154,4 @@ export interface CheckByte {
      * Customizable based on encoding.
      */
     greater0xffffByte: number;
-}
-export interface Grid extends GridMethods, HTMLElement{
-    _id: string;
-    _gridInfo: GridInfo;
-    _gridCssInfo: GridCssInfo;
-    _colInfos: ColInfo[];
-    _defaultColInfo: DefaultColInfo;
-    _variables: GridVariables;
-    _events: {
-        onActiveCell(row: number, colId: string): boolean;
-        onActiveCells(startRow: number, startColId: string, endRow: number, endColId: string): boolean;
-        onActiveRow(row: number): boolean;
-        onActiveRows(startRow: number, endRow: number): boolean;
-        onActiveCol(colId: string): boolean;
-        onActiveCols(startColId: string, endColId: string): boolean;
-        onBeforeChange(row: number, colId: string, oldValue: any, newValue: any): boolean;
-        onAfterChange(row: number, colId: string, oldValue: any, newValue: any): void;
-        onBeforeClickCell(row: number, colId: string): boolean;
-        onAfterClickCell(row: number, colId: string): void;
-        onClickSelect(row: number, colId: string, selectNode: HTMLElement): boolean;
-        onClickCheckbox(row: number, colId: string, checkboxNode: HTMLElement): boolean;
-        onClickButton(row: number, colId: string, buttonNude: HTMLElement): boolean;
-        onClickLink(row: number, colId: string, linkNode: HTMLElement): boolean;
-        onBeforeDblClickCell(row: number, colId: string): boolean;
-        onAfterDblClickCell(row: number, colId: string): void;
-        onBeforeClickHeader(row: number, colId: string): boolean;
-        onAfterClickHeader(row: number, colId: string): void;
-        onBeforeDblClickHeader(row: number, colId: string): boolean;
-        onAfterDblClickHeader(row: number, colId: string): void;
-        onBeforeEditEnter(row: number, colId: string, editorNode: HTMLElement): boolean;
-        onAfterEditEnter(row: number, colId: string, editorNode: HTMLElement): void;
-        onEditEnding(row: number, colId: string, oldValue: any, newValue: any): boolean;
-        onClickFilter(row: number, colId: string, filterNode: HTMLElement): boolean;
-        onChooseFilter(row: number, colId: string, oldValue: any, newValue: any): boolean;
-        onPaste(startRow: number, startColId: string, clipboardText: string): boolean;
-        onCopy(startRow: number, startColId: string, endRow: number, endColId: string, copyText: string): boolean;
-        onResize(colId: string): boolean;
-        onKeydownEditor(event: KeyboardEvent): boolean;
-        onInputEditor(event: InputEvent): boolean;
-        onKeydownGrid(event: KeyboardEvent): boolean;
-    };
-    gridHeader: GridHeader;
-    gridBody: GridBody;
-    gridFooter: GridFooter;
-    _vg: Vanillagrid;
-}
-interface GridVariables {
-    _activeRows: number[];
-    _activeCols: number[];
-    _activeCells: Cell[];
-    _targetCell: Cell | null;
-    _records: CellRecord[][];
-    _recordseq: number;
-    _sortToggle: Record<string, boolean>;
-    _filters: { colId : string, value : string }[];
-    _isDrawable: boolean;
-}
-export interface GridHeader extends HTMLElement{
-    _gridId: string;
-    _type: string;
-    _gridHeaderCells: Cell[][];
-}
-export interface GridBody extends HTMLElement{
-    _gridId: string;
-    _type: string;
-    _gridBodyCells: Cell[][];
-}
-export interface GridFooter extends HTMLElement{
-    _gridId: string;
-    _type: string;
-    _gridFooterCells: Cell[][];
 }
