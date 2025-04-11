13635659942864d7132d9df72473a5a2d5759cc7
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-11 16:53:47 +0900

    Update VanillagridTest.vue

--- playground/src/components/VanillagridTest.vue
+++ playground/src/components/VanillagridTest.vue
@@ -2,23 +2,23 @@
   <div ref="gridWrap">
     <button @click="setGrid1Data">getGridData 1</button>
     <button @click="setGrid2Data">setGridData 3</button>
-    <div data-vanillagrid id="grid1" height="200px">
+    <div data-vanillagrid data-id="grid1" height="200px">
         <div data-col id="col1" header="header" data-type="text" width="600"></div>
     </div>
-    <div data-vanillagrid id="grid2" locked-color="false" height="400px">
-        <div data-col id="dept" header="Click the Sigma button to the left of the header;filter1(mask);dept " data-type="mask" format="AAA99" width="100" locked="true"></div>
+    <div data-vanillagrid data-id="grid2" locked-color="false" height="400px">
+        <div data-col id="dept" header="Click the Sigma button to the left of the header;filter1(mask);dept " data-type="mask" format="AAA99" width="100" locked="true" row-merge="true"></div>
         <div data-col id="f_nm" header=";filter2(text);first name" data-type="text" width="120" align="center" locked="true"></div>
-        <div data-col id="l_nm" header=";filter3(text);last name" data-type="text" width="120" align="center" locked="true"></div>
+        <div data-col id="l_nm" header=";filter3(text);last name" data-type="text" width="120" align="center" locked="true" col-merge="true"></div>
         <div data-col id="d_o_j" header=";filter4(month);DOJ" data-type="month" width="120" locked="true"></div>
-        <div data-col id="e_id" header="Please double click;sort1(text);Employee ID" data-type="text" width="120" align="center" locked="true" footer="MAX;MIN;AVG;SUM"></div>
-        <div data-col id="salary" header=";sort2(number);salary" data-type="number" format="$ #,###.00" width="150" footer="$$MAX;$$MIN;$$AVG;$$SUM"></div>
-        <div data-col id="status" header=";checkbox;status" data-type="checkbox" width="80"></div>
+        <div data-col id="e_id" header="sort1(text);;" data-type="text" width="120" align="center" locked="true" footer="MAX;MIN;AVG;SUM"></div>
+        <div data-col id="salary" header="sort2(number);;salary" data-type="number" format="$ #,###.00" width="150" footer="$$MAX;$$MIN;$$AVG;$$SUM"></div>
+        <div data-col id="status" header="Please double click;checkbox;status" data-type="checkbox" width="80"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { Vanillagrid, Grid } from 'vanillagrid2';
+import type { Vanillagrid, GridMethods } from 'vanillagrid2';
 import { ref } from 'vue';
 import { getCurrentInstance, onMounted, onBeforeUnmount } from 'vue';
 
@@ -27,7 +27,7 @@ const vg: Vanillagrid = (proxy as any)!.$vg;
 const gridWrap = ref<HTMLElement | null>(null);
 
 const setGrid1Data = () => {
-  const grid: Grid = vg.getGrid("grid1")!;
+  const grid: GridMethods = vg.getGrid("grid1")!;
   const data = [
       {
           col1 : 'This is a text dataType.\nDouble-click to create an editor.\nPress F2 to create an editor.\nWhen you press the enter key, editor focus.\nTry it!',
@@ -49,7 +49,7 @@ const setGrid2Data = () => {
       },
       {
           dept : "AAA01",
-          f_nm : "Robert",
+          f_nm : "Johnson",
           l_nm : "Johnson",
           d_o_j : "200103",
           e_id : "2",
@@ -67,7 +67,7 @@ const setGrid2Data = () => {
       },
       {
           dept : "AAA01",
-          f_nm : "Thomas",
+          f_nm : "Smith",
           l_nm : "Smith",
           d_o_j : "202403",
           e_id : "46",
@@ -219,7 +219,7 @@ const setGrid2Data = () => {
           status : "Y",
       },
   ];
-  const grid: Grid = vg.getGrid("grid2")!;
+  const grid: GridMethods = vg.getGrid("grid2")!;
   console.log(grid);
   grid.load(data);
 };
