1f12195d47cf02c06c7bf4f36d66a2725f2dd3c1
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-11 16:55:11 +0900

    Update unmountVanillagrid.ts

--- src/core/unmountVanillagrid.ts
+++ src/core/unmountVanillagrid.ts
@@ -1,14 +1,14 @@
 import type { Vanillagrid } from "../types/vanillagrid";
 import type { Grid } from "../types/grid";
+import { removeAllChild } from "../utils/utils";
 
 export const unmountVanillagrid = (vg: Vanillagrid, gridList: Record<string, Grid>, targetElement?: HTMLElement) => {
     if(!vg._initialized) throw new Error('Please initialize vanillagrid');
     const targetEl = targetElement ? targetElement : document;
-    const vanillagrids: NodeListOf<Grid> = targetEl.querySelectorAll('vanilla-grid');
+    const vanillagridBoxList: NodeListOf<HTMLElement> = targetEl.querySelectorAll('[data-vanillagrid]');
 
-    vanillagrids.forEach((grid: Grid | null) => {
-        grid!.parentNode!.removeChild(grid!);
-        delete vg.grids[grid!._id];
-        grid = null;
+    vanillagridBoxList.forEach((vanillagridBox: any) => {
+        removeAllChild(vanillagridBox);
+        delete gridList[vanillagridBox._gridId];
     });
 }
