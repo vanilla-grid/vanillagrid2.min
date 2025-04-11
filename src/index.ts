776e055744d3a913dd8ecf31847c9a1ab2c66164
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-11 16:57:37 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export type { ColInfo, DefaultColInfo } from "./types/colInfo";
 export type { Cell, CellData, CellRecord } from "./types/cell";
 export type { GridMethods } from "./types/gridMethods";
 export type { DataType } from "./types/dataType";
+export type { GridEvents } from "./types/grid";
 export type { documentEvent } from "./types/event";
 export { Align, ColorSet, SelectionPolicy, VerticalAlign, alignUnit, enumWidthUnit, footerUnit, selectionPolicyUnit, statusUnit, verticalAlignUnit } from "./types/enum";
 
