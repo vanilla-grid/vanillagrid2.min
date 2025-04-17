e0c810147759af42c30cd750931a9826be88b0b4
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-17 14:04:46 +0900

    Update unmountVanillagrid.ts

--- src/core/unmountVanillagrid.ts
+++ src/core/unmountVanillagrid.ts
@@ -8,7 +8,21 @@ export const unmountVanillagrid = (vg: Vanillagrid, gridList: Record<string, Gri
     const vanillagridBoxList: NodeListOf<HTMLElement> = targetEl.querySelectorAll('[data-vanillagrid]');
 
     vanillagridBoxList.forEach((vanillagridBox: any) => {
-        removeAllChild(vanillagridBox);
-        delete gridList[vanillagridBox._gridId];
+        const grid = gridList[vanillagridBox._gridId];
+        if(grid._isMounted) {
+            grid.elements.gridHeader.removeEventListener('dblclick', grid.hendler.gridHeader_dblclick);
+            grid.elements.gridHeader.removeEventListener('click', grid.hendler.gridHeader_click);
+            grid.elements.gridBody.removeEventListener('mousemove', grid.hendler.gridBody_mousemove);
+            grid.elements.gridBody.removeEventListener('mouseleave', grid.hendler.gridBody_mouseleave);
+            grid.elements.gridBody.removeEventListener('mouseenter', grid.hendler.gridBody_mouseenter);
+            grid.elements.gridBody.removeEventListener('dblclick', grid.hendler.gridBody_dblclick);
+            grid.elements.grid.removeEventListener('click', grid.hendler.gridElement_click);
+            grid.elements.grid.removeEventListener('mousedown', grid.hendler.gridElement_mousedown);
+            grid.elements.grid.removeEventListener('mousemove', grid.hendler.gridElement_mousemove);
+            grid.elements.grid.removeEventListener('mouseleave', grid.hendler.gridElement_mouseleave);
+            
+            removeAllChild(vanillagridBox);
+            delete gridList[vanillagridBox._gridId];
+        }
     });
-}
+};
